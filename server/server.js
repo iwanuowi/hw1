07e64b86e14a29f4cba652d1d5f3b100838c9095
Ivan <ivan06owi@gmail.com>
2025-08-19 01:11:23 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/ecommerce");
+    console.log("MongoDB is Connected");
+  } catch (error) {
+    console.log(" MongoDB connection failed:", error);
+  }
+}
+
+const productRouter = require("./routes/product");
+
+app.use("/products", productRouter);
+
+connectToMongoDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the E-Commerce! 🚀");
+});
+
+app.listen(8888, () => {
+  console.log("Your server is running on http://localhost:8888");
+});
